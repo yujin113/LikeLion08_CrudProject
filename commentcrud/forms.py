4fb045cfd5a2e931ebc34a7005fdc7a1171c709b
yujin113 <claire9585@sookmyung.ac.kr>
2021-07-12 14:51:20 +0900

    댓글 작성, 수정 부분 form 속성 추가

--- commentcrud/forms.py
+++ commentcrud/forms.py
@@ -5,3 +5,9 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.TextInput(attrs={'placeholder': '댓글을 작성해주세요'}),
+        }
+        labels = {
+            'content': '입력 후 엔터 '
+        }
